582d3b50204ba10e79fec74462e08531d08ee19d
Jessie Ssebuliba <jessiessebuliba@gmail.com>
2025-09-30 11:43:21 +0300

    load synchronously
    Signed-off-by: Jessie Ssebuliba <jessiessebuliba@gmail.com>

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,6 @@
 // A dependency graph that contains any wasm must all be imported
 // asynchronously. This `bootstrap.js` file does the single async import, so
 // that no one else needs to worry about it again.
-import("./index.js").catch((e) =>
-  console.error("Error importing `index.js`:", e)
-);
+
+// Safe synchronous import so Webpack bundles it correctly in both dev + prod
+import "./index.js";
